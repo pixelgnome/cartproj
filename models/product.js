51fcb5618463ffb7e0a1418dc5a88a5dcebc5063
Salvatore Filippone <sfilippone@gmail.com>
2025-06-01 14:59:35 -0700

    finished model module

--- models/product.js
+++ models/product.js
@@ -1,20 +1,42 @@
-const products = [];
+// Let's save to files instead of an array
+// we have to import file system and path in order to save to local
+const fs = require("fs");
+const path = require("path");
+
+ const p = path.join(
+    path.dirname(require.main.filename),
+    "data",
+    "products.json"
+  );
+  
+const getProductsFromFile = (cb) => {
+ 
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+    
+  });
+};
 
 module.exports = class Product {
+  // Builds the product info
+  constructor(t) {
+    this.title = t;
+  }
 
-    // Builds the product info
-    constructor(t) {
-        this.title = t;
-    }
-
-    // stores the product info to array (soon to DB)
-    save() {
-        products.push(this);
-    }
-
-    static fetchAll() {
-        return products;
-    }
-
-
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
 }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
