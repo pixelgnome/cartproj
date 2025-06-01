c2244aa3d5014cd12bc8fd99460a33e684692218
Salvatore Filippone <sfilippone@gmail.com>
2025-06-01 13:54:03 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const port = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const errorController = require('./controllers/error');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.error404);
+
+app.listen(port, ()=> {
+    console.log(`Listening on ${port}`);
+})
