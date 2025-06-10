93f572c926b9dcacfa7b165a21e4f5883582d819
Salvatore Filippone <sfilippone@gmail.com>
2025-06-10 10:38:51 -0700

    added new model: user, created dummey user via sequelize

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 //Import database
 const sequelize = require("./util/database");
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const port = 3000;
 
 const app = express();
@@ -19,15 +22,47 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+  .then(user => {
+   req.user = user;
+   next();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
+
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Associations...
+Product.belongsTo(User, {
+  constraints: true,
+  onDelete: 'CASCADE'
+});
+User.hasMany(Product);
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((result) => {
-    // console.log(result);
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if(!user) {
+      return User.create({
+        name: 'Max',
+        email: 'test@test.com'
+      });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
     });
