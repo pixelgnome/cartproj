718f2c04adefa27d0233d5a48c26a729cba802f6
Salvatore Filippone <sfilippone@gmail.com>
2025-06-13 10:52:41 -0700

    bongo bongo

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'turiddu!', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
+  MongoClient.connect(
+    "mongodb+srv://rdnzl:testes123@cluster0.33wzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((result) => {
+      console.log("Connected to cluster");
+    })
+    .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
